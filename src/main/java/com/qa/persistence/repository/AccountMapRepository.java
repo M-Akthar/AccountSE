19509271e1fa2d3b508e22702f28d348c4ce392f
M-Akthar <Muhammad.Aktharuzzaman@academytrainee.com>
2019-07-16 15:57:54 +0100

    icommit, Implemented methods within AccountMapRepository

--- src/main/java/com/qa/persistence/repository/AccountMapRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountMapRepository.java
@@ -3,11 +3,16 @@ package com.qa.persistence.repository;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.security.auth.login.AccountNotFoundException;
+
 import com.qa.persistence.domain.Account;
+import com.qa.util.JSONUtil;
 
 public class AccountMapRepository implements AccountRepository{
 	
 	Map<Integer, Account> accountMap = new HashMap<Integer, Account>();
+	
+	private int count = 1;
 	
 	//You must provide concrete implementation for each of these methods
 	//do not change the method signature
@@ -20,22 +25,75 @@ public class AccountMapRepository implements AccountRepository{
 
 	public String getAllAccounts() {
 		// TODO Auto-generated method stub
-		return null;
+		String message = "All accounts output to console";
+		
+		for(int id : accountMap.keySet()) {
+			System.out.println("---- Account ----");
+			System.out.println("ID: " + id);
+			System.out.println("Account: " + accountMap.get(id).getAccountNumber());
+			System.out.println("First Name: " + accountMap.get(id).getFirstName());
+			System.out.println("Last Name: " + accountMap.get(id).getLastName());
+		}
+		
+		return message;
 	}
 
-	public String createAccount(String account) {
+	public String createAccount(int accountNumber, String firstName, String lastName) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		Account acc = new Account(accountNumber, firstName, lastName);
+		acc.setId(count);
+		
+		accountMap.put(acc.getId(), acc);
+		
+		String message = firstName + " " + lastName + " added to account map.";
+		count++;
+		return message;
 	}
 
-	public String deleteAccount(int accountNumber) {
+	public String deleteAccount(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		String message = "";
+		
+		if(accountMap.get(id) != null) {
+			
+			String firstName = accountMap.get(id).getFirstName();
+			String lastName = accountMap.get(id).getLastName();
+			
+			accountMap.remove(id);
+			
+			message = firstName + " " + lastName + " removed from account map.";
+		} else {
+			message = "Account not found, please check your id";
+		}	
+		
+		return message;
 	}
 
-	public String updateAccount(int accountNumber, String account) {
+	public String updateAccount(int id, int accountNumber, String firstName, String lastName) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		String message = "";
+		
+		if(accountMap.get(id) != null) {
+			
+			accountMap.get(id).setAccountNumber(accountNumber);
+			accountMap.get(id).setFirstName(firstName);
+			accountMap.get(id).setLastName(lastName);
+
+			message = "The account for " + firstName + " " + lastName + " has been updated.";
+		} else {
+			message = "Account not found, please check your id";
+		}
+
+//		I can use a conditional to check if the account number has changed. If the account number
+//		is the same then I can simply do account.put to replace the values using the existing key.
+		
+//		deleteAccount(accountNumber);
+//		createAccount(newAccountNumber, firstName, lastName);
+		
+		return message;
 	}
 
 }
