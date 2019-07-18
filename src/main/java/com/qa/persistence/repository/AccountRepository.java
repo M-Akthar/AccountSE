9857ebfc66db5761fe8d86951068cc7cd4427a72
M-Akthar <Muhammad.Aktharuzzaman@academytrainee.com>
2019-07-18 11:17:15 +0100

    Updated Tests, Added JSON conversion to methods and a DatabaseRepository

--- src/main/java/com/qa/persistence/repository/AccountRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountRepository.java
@@ -3,8 +3,8 @@ package com.qa.persistence.repository;
 public interface AccountRepository {
 
 	String getAllAccounts();
-	String createAccount(int accountNumber, String firstName, String lastName);
-	String deleteAccount(int id);
+	String createAccount(String account);
+	String deleteAccount(String account);
 	String updateAccount(int id, int accountNumber, String firstName, String lastName);
 
 }
